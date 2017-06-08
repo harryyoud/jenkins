d0db1a6426c336c0bfcabd232623d54e4840deb2
Harry Youd <harry@harryyoud.co.uk>
2017-06-08 17:52:45 +0100

    Add WITH_DEXPREOPT and reorganise

--- lineage-build-kicker.groovy
+++ lineage-build-kicker.groovy
@@ -11,11 +11,12 @@ node("master"){
 		build job: 'lineage-14.1', parameters: [
 			string(name: 'DEVICE', value: (json[i].device == null) ? "HELP-omgwtfbbq" : json[i].device),
 			string(name: 'BUILD_TYPE', value: (json[i].build_type == null) ? "userdebug" : json[i].build_type),
-			string(name: 'OTA', value: 'true'),
 			string(name: 'REPOPICK_NUMBERS', value: (json[i].repopick_nums == null) ? "" : json[i].repopick_nums),
 			string(name: 'REPOPICK_TOPICS', value: (json[i].repopick_tops == null) ? "" : json[i].repopick_tops),
 			string(name: 'WITH_SU', value: (json[i].with_su == null) ? "false" : json[i].with_su),
 			string(name: 'WITH_GAPPS', value: (json[i].with_gapps == null) ? "false" : json[i].with_gapps),
+			string(name: 'WITH_DEXPREOPT', value: (json[i].with_dexpreopt == null) ? "false" : json[i].with_dexpreopt),
+			string(name: 'OTA', value: (json[i].ota == null) ? "false" : json[i].ota),
 			string(name: 'CRON_RUN', value: 'true')
 		], propagate: false, wait: false
 		sleep 2
