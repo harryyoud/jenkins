7834918e3618fb2f364eafc56be98d9a0c1b6601
Harry Youd <harry@harryyoud.co.uk>
2017-07-12 08:38:24 +0100

    Adapt build kicker for new name

--- lineage-build-kicker.groovy
+++ lineage-build-kicker.groovy
@@ -15,7 +15,7 @@ node("master"){
             }
         }
         echo "Kicking off a build for ${json[i].device}"
-        build job: 'lineage-14.1', parameters: [
+        build job: 'lineage-build', parameters: [
             string(name: 'VERSION', value: (json[i].version == null) ? "14.1" : json[i].version),
             string(name: 'DEVICE', value: (json[i].device == null) ? "HELP-omgwtfbbq" : json[i].device),
             string(name: 'BUILD_TYPE', value: (json[i].build_type == null) ? "userdebug" : json[i].build_type),
