3c6cf7777faf25bf1fef77ed7802d64cc514cb49
Harry Youd <harry@harryyoud.co.uk>
2017-07-30 02:10:40 +0100

    Send Slack notifications

---
+++ includes/slack-send.groovy
@@ -0,0 +1,21 @@
+def notifySlack(String buildStatus = 'STARTED') {
+    // Build status of null means success.
+    buildStatus = buildStatus ?: 'SUCCESS'
+    def color
+
+    if (buildStatus == 'STARTED') {
+        color = '#D4DADF'
+    } else if (buildStatus == 'SUCCESS') {
+        color = '#BDFFC3'
+    } else if (buildStatus == 'UNSTABLE') {
+        color = '#FFFE89'
+    } else {
+        color = '#FF9FA1'
+    }
+
+    def msg = "${buildStatus}: `${env.JOB_NAME} - ${currentBuild.displayName}` :\n${env.BUILD_URL}"
+
+    slackSend(color: color, message: msg)
+}
+
+return this
--- jobs/lineage-mirror-sync.groovy
+++ jobs/lineage-mirror-sync.groovy
@@ -1,6 +1,16 @@
+def slack
+node("master"){ slack = load "${workspace}@script/includes/slack-send.groovy" }
+
 node("the-revenge"){
-  sh '''#!/bin/bash +x
-    cd /mnt/Data/AndroidMirror/
-    repo sync -j16
-  '''
+  try {
+    sh '''#!/bin/bash +x
+      cd /mnt/Data/AndroidMirror/
+      repo sync -j16
+    '''
+  } catch (e) {
+    currentBuild.result = "FAILED"
+    throw e
+  } finally {
+    slack.notifySlack(currentBuild.result)
+  }
 }
