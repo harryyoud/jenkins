dcf223b49577e660a404ea682a6138259ff08d81
Harry Youd <harry@harryyoud.co.uk>
2017-10-26 18:59:45 +0100

    Install change-id

--- jobs/lineage-mirror-manifest.groovy
+++ jobs/lineage-mirror-manifest.groovy
@@ -28,6 +28,7 @@ node("master"){
         diff default.xml old-default.xml > /dev/null
         if [ $? = 1 ]; then
           git add default.xml
+          gitdir=$(git rev-parse --git-dir); scp -p -P 29418 harryyoud@review.lineageos.org:hooks/commit-msg ${gitdir}/hooks/
           git commit -m "Updated to $(printf "%(%d-%b-%Y)T\\n" -1) $(date -u +%H:%M:%S) UTC" --author "Harry's Buildbot <buildbot@harryyoud.co.uk>"
           git push ssh://harryyoud@review.lineageos.org:29418/LineageOS/mirror HEAD:refs/for/master%r=timschumi2@arcor.de
         else
