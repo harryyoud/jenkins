2cf3d17ad0dcd9149c0a002709f1fda1b2b2f8de
Harry Youd <harry@harryyoud.co.uk>
2018-02-18 18:16:46 +0100

    Copy changes from www to wiki

--- jobs/lineage-wiki-preview.groovy
+++ jobs/lineage-wiki-preview.groovy
@@ -3,21 +3,24 @@ node("build"){
 			git url:'https://github.com/LineageOS/lineage_wiki'
 	}
 	stage('Pick commit'){
-		sh '''
-			python - $CHANGE <<-END
+		withCredentials([string(credentialsId: 'LineageGerritHTTPPass', variable: 'GERRITHTTPPASS')]) {
+			sh '''
+				python - $CHANGE $GERRITHTTPPASS<<-END
 				from pygerrit2.rest import GerritRestAPI
 				from sys import argv
 				from os import system
+				from requests.auth import HTTPBasicAuth
 				gerrit_url = "https://review.lineageos.org/"
-				rest = GerritRestAPI(url=gerrit_url)
+				auth = HTTPBasicAuth('harryyoud', argv[2])
+				rest = GerritRestAPI(url=gerrit_url, auth=auth)
 				change = rest.get("/changes/{}?o=DOWNLOAD_COMMANDS&o=CURRENT_REVISION".format(argv[1]))
-				commands = change['revisions'].items()[0][1]['fetch']['anonymous http']
-				url = commands['url']
+				commands = change['revisions'].items()[0][1]['fetch'].itervalues().next()
 				ref = commands['ref']
-				system('git fetch {} {} && git checkout FETCH_HEAD'.format(url, ref))
+				rest.post("/changes/{}/reviewers".format(argv[1]), json={"reviewer": "harry-jenkins", "notify": "NONE"})
+				system('git fetch https://github.com/LineageOS/lineage_wiki {} && git checkout FETCH_HEAD'.format(ref))
 				END
-			gpick 203329
-		'''
+			'''
+		}
 	}
 	stage('Go'){
 		sh '''#!/bin/bash
@@ -36,7 +39,7 @@ node("build"){
 			if [ $? == 0 ]; then
 				ssh -p 29418 harry-jenkins@review.lineageos.org gerrit review -n OWNER --label Verified=+1 -m \\'"Build successful for change $CHANGE, patchset $PATCHSET; validation passed. Preview available at https://harryyoud.co.uk/lineage-previews/$CHANGE/$PATCHSET"\\' $CHANGE,$PATCHSET
 			else
-				ssh -p 29418 harry-jenkins@review.lineageos.org gerrit review -n OWNER--label Verified=-1 -m \\'"Build failed for change $CHANGE, patchset $PATCHSET. View the log at ${BUILD_URL}console"\\' $CHANGE,$PATCHSET
+				ssh -p 29418 harry-jenkins@review.lineageos.org gerrit review -n OWNER --label Verified=-1 -m \\'"Build failed for change $CHANGE, patchset $PATCHSET. View the log at ${BUILD_URL}console"\\' $CHANGE,$PATCHSET
 			fi
 			mkdir -p /home/www/nginx/sites/harryyoud.co.uk/public_html/lineage-previews/$CHANGE/$PATCHSET
 			rsync -vr _site/ /home/www/nginx/sites/harryyoud.co.uk/public_html/lineage-previews/$CHANGE/$PATCHSET --delete --exclude .well-known --exclude images/devices
