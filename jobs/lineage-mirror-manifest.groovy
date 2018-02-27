0c975c07962e7949803da15a9051e5306c1e2eb4
Harry Youd <harry@harryyoud.co.uk>
2018-02-27 23:19:08 +0000

    Use stages to allow usage of post actions

--- jobs/lineage-mirror-manifest.groovy
+++ jobs/lineage-mirror-manifest.groovy
@@ -2,38 +2,40 @@ def slack
 node("master"){ slack = load "${workspace}@script/includes/slack-send.groovy" }
 
 node("build"){
-  stage('Clone'){
-    git url:'https://github.com/LineageOS/mirror'
-  }
-  stage('Generate mirror'){
-    withCredentials([string(credentialsId: 'GitHubAPIKey', variable: 'GHTOKEN')]) {
+  stages {
+    stage('Clone'){
+      git url:'https://github.com/LineageOS/mirror'
+    }
+    stage('Generate mirror'){
+      withCredentials([string(credentialsId: 'GitHubAPIKey', variable: 'GHTOKEN')]) {
+        sh '''#!/bin/bash +x
+          mv default.xml old-default.xml
+          export GHUSER="harryyoud"
+          python3 ./mirror-regen.py
+        '''
+      }
+    }
+    stage('Compare'){
       sh '''#!/bin/bash +x
-        mv default.xml old-default.xml
-        export GHUSER="harryyoud"
-        python3 ./mirror-regen.py
+        diff default.xml old-default.xml
+        if [ $? = 1 ]; then
+          echo "New manifest differs from previous"
+        fi
       '''
     }
-  }
-  stage('Compare'){
-    sh '''#!/bin/bash +x
-      diff default.xml old-default.xml
-      if [ $? = 1 ]; then
-        echo "New manifest differs from previous"
-      fi
-    '''
-  }
-  stage('Create draft change'){
-    sh '''#!/bin/bash +x
-      diff default.xml old-default.xml > /dev/null
-      if [ $? = 1 ]; then
-        git add default.xml
-        gitdir=$(git rev-parse --git-dir); scp -p -P 29418 harryyoud@review.lineageos.org:hooks/commit-msg ${gitdir}/hooks/
-        git commit -m "Updated to $(printf "%(%d-%b-%Y)T\\n" -1) $(date -u +%H:%M:%S) UTC" --author "Harry's Buildbot <buildbot@harryyoud.co.uk>"
-        git push ssh://harryyoud@review.lineageos.org:29418/LineageOS/mirror HEAD:refs/for/master
-      else
-        echo "No changes, skipping"
-      fi
-    '''
+    stage('Create draft change'){
+      sh '''#!/bin/bash +x
+        diff default.xml old-default.xml > /dev/null
+        if [ $? = 1 ]; then
+          git add default.xml
+          gitdir=$(git rev-parse --git-dir); scp -p -P 29418 harryyoud@review.lineageos.org:hooks/commit-msg ${gitdir}/hooks/
+          git commit -m "Updated to $(printf "%(%d-%b-%Y)T\\n" -1) $(date -u +%H:%M:%S) UTC" --author "Harry's Buildbot <buildbot@harryyoud.co.uk>"
+          git push ssh://harryyoud@review.lineageos.org:29418/LineageOS/mirror HEAD:refs/for/master
+        else
+          echo "No changes, skipping"
+        fi
+      '''
+    }
   }
   post {
     always {
